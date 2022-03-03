5f5051b6371e9ce49c6e3d3c442bc04e8e04064d
Ivan Rybachok <blackhusky2014@gmail.com>
2022-03-03 09:30:25 -0400

    issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          {/* add something else in here @ivan-rybachok todo #2 */}
         </p>
         <a
           className="App-link"
